143c88c8820152d51c298c56daff9c74257f22de
kyungh103 <kyungh103@gmail.com>
2019-12-12 21:26:16 +0900

    클래스 배열

---
+++ Java/java_algorithm/src/chap02/PhysicalExamination.java
@@ -0,0 +1,66 @@
+package chap02;
+
+import java.util.Scanner;
+
+public class PhysicalExamination {
+	static final int VMAX = 21;//시력 분포(0.0에서 0.1 단위로 21개)
+	
+	static class PhyscData{
+		String name;
+		int height;
+		double vision;
+		
+		PhyscData(String name,int height,double vision){
+			this.name = name;
+			this.height = height;
+			this.vision = vision;
+		}
+	}
+	
+	static double aveHeight(PhyscData[] dat) {
+		double sum = 0;
+		 
+		for(int i =0; i < dat.length; i++) {
+			sum += dat[i].height;
+		}
+		return sum /dat.length;
+	}
+	static void distVision(PhyscData[] dat, int[] dist) {
+		int i =0;
+		dist[i]=0;
+		for(i=0;i<dat.length;i++) {
+			if(dat[i].vision >= 0.0 && dat[i].vision <= VMAX/10.0)
+				dist[(int)(dat[i].vision * 10)]++;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner stdln = new Scanner(System.in);
+		
+		PhyscData[] x= {
+			new PhyscData("홍길동",162,0.3),
+			new PhyscData("김길동",173,0.7),
+			new PhyscData("박길동",175,2.0),
+			new PhyscData("이길동",158,1.3),
+			new PhyscData("최길동",170,1.5),
+			new PhyscData("안길동",168,1.2),
+			new PhyscData("유길동",172,0.8),
+		};
+		int[] vdist = new int[VMAX];
+		
+		System.out.println("신체검사 리스트");
+		System.out.println("이름        키   시력");
+		System.out.println("----------------");
+		for(int i=0;i < x.length; i++)
+			System.out.printf("%-8s%3d%5.1f\n", x[i].name,x[i].height,x[i].vision);
+		
+		System.out.printf("\n평균 키:%5.1fcm\n", aveHeight(x));
+		
+		distVision(x, vdist);
+		
+		System.out.println("\n시력분포");
+		for(int i=0;i < VMAX; i++) {
+			System.out.printf("%3.1f~:%2d명\n", i/10.0, vdist[i]);			
+		}
+		
+	}
+}
